320f254aa510d45c723137d3c6af06622f2e7e74
Murto Hilali <hilali.murto@gmail.com>
2022-09-01 16:46:32 +0000

    Updates to easy_cluster workflow

---
+++ README.md
@@ -0,0 +1,27 @@
+# MMseqs2: easy-cluster
+
+easy-cluster by default clusters the entries of a FASTA/FASTQ file using a cascaded clustering algorithm.
+
+### Usage
+
+This workflow returns 3 files:
+
+* ```{prefix}_all_seqs.fasta```
+* ```{prefix}_cluster.tsv```
+* ```{prefix}_rep_seq.fasta```
+
+Where ```{prefix}``` may be set by the user.
+
+MMseqs2 (Many-against-Many searching) is a software suite to search and cluster huge sequence sets. MMseqs2 is open source GPL-licensed software implemented in C++ for Linux, Mac OS and Windows.
+
+### Arguments
+
+* __fastaq1__: Bam file to take metrics from.
+* __outdir__: Latch console destination for output files.
+* __prefix__: Name for output FASTQ files.
+
+### Dependencies
+
+* [mmseqs2](https://github.com/soedinglab/MMseqs2)
+* [conda](https://docs.conda.io/)
+* [LatchBio](https://latch.bio/)
--- wf/__init__.py
+++ wf/__init__.py
@@ -3,46 +3,91 @@
 #Boilerplate
 import subprocess
 from pathlib import Path
+import os
 
-from latch import small_task, workflow
+from latch import medium_task, workflow
 from latch.types import LatchFile
 from latch.types import LatchDir
 
-#Place all input file parameters into an ordered list that is fed to subprocess
+@medium_task
+def start_cluster(fastaq1: LatchFile, outdir: LatchDir, prefix: str = "test") -> LatchDir: 
 
-@small_task
-def start_cluster(fastaq1: LatchFile) -> LatchFile: 
-    
+    tmp_dir = Path("/root/tmp/").resolve()
+    os.mkdir("/root/results/")
+    local_dir = Path("/root/results/").resolve()
+
     #Exact command line args that would be used in terminal
     easycluster_cmd = [
         "mmseqs",
         "easy-cluster",
-        fastaq1.local_path,
-        "clusterRes",
-        "tmp"
+        str(fastaq1.local_path),
+        prefix,
+        str(tmp_dir)
+    ]
+
+    _mv_cmd = [
+        "mv",
+        f"/root/{prefix}_all_seqs.fasta",
+        f"/root/{prefix}_cluster.tsv",
+        f"/root/{prefix}_rep_seq.fasta",
+        str(local_dir)
     ]
 
     subprocess.run(easycluster_cmd)
+    subprocess.run(_mv_cmd)
+    
+    return LatchDir(str(local_dir), outdir.remote_path)
+    #return LatchDir("/root/", outdir.remote_path)
 
-    return LatchFile("latch:///easycluster.sh")
 
 @workflow
-def easycluster(fastaq1: LatchFile) -> LatchFile:
+def easy_cluster(fastaq1: LatchFile, outdir: LatchDir, prefix: str = "test") -> LatchDir:
     """Perform quick clustering on a FASTA/FASTQ entry.
-    ----
-    > Regular markdown constructs work as expected.
-    # Mmseqs2: easy-cluster workflow
     
+    MMseqs2: easy-cluster
+    ----
+    ### Description
+
+    easy-cluster by default clusters the entries of a FASTA/FASTQ file 
+    using a cascaded clustering algorithm.
+
+    This workflow returns 3 files:
+
+    * ```{prefix}_all_seqs.fasta```
+    * ```{prefix}_cluster.tsv```
+    * ```{prefix}_rep_seq.fasta```
+
+    Where ```{prefix}``` may be set by the user.
+
+    MMseqs2 (Many-against-Many searching) is a software suite to search 
+    and cluster huge sequence sets. MMseqs2 is open source GPL-licensed 
+    software implemented in C++ for Linux, Mac OS and Windows.
+
     __metadata__:
-        display_name: Mmseqs2 easy-search Workflow (Latch adaptation)
+        display_name: easy_cluster
         author:
-            name: Michael Trịnh 
-            email: michaeltrinh19@gmail.com
+            name: Michael Trịnh & Murto Hilali
+            email: michaeltrinh19@gmail.com & hilali.murto@gmail.com
             github: https://github.com/MtrinhO
         repository: https://github.com/MtrinhO/MMSeqs-on-LatchBio/tree/easycluster
         license: GNU General Public License v3.0
     Args:
         fastaq1:
-          Your FASTA sequence to be clustered
+          Your FASTA/FASTQ sequence to be clustered.
+
+          __metadata__:
+            display_name: FASTA/FASTQ File
+
+        outdir:
+          Your output directory.
+
+          __metadata__:
+            display_name: Output Directory
+
+        prefix:
+          Output file name prefix.
+
+          __metadata__:
+            display_name: Filename Prefix
     """
-    return start_cluster(fastaq1=fastaq1)
+    return start_cluster(fastaq1=fastaq1, outdir=outdir, prefix=prefix)
